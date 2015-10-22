162ebdfe194fbca60c596abaaed5e382a2c17690
tega <tega.philip@gmail.com>
2015-10-22 19:27:14 +0200

    TER-2 >> Completed user registration and password generation function. Awaiting testing

--- Libraries/Utils.php
+++ Libraries/Utils.php
@@ -33,9 +33,39 @@ class Utils
      * @param string $dbHash the hashed password that was saved
      * @return bool
      */
-    public function verifyPassword($rawPassword, $dbHash)
+    public static function verifyPassword($rawPassword, $dbHash)
     {
         //todo test this with many randomly generated passwords for vulnerabilities.
         return password_verify($rawPassword, $dbHash);
+    }
+
+
+    /**
+     * Function to generate a random password
+     * @param int $length
+     * @param bool|true $strict
+     * @return string
+     */
+    public static function generateRandomPassword($length = 8, $strict = true)
+    {
+        $passwordArray = [];
+
+        $randomString = str_shuffle("abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ1234567890");
+
+        for ($i = 0; $i < $length; $i++) {
+            $n = rand(0, strlen($randomString) - 1);
+            $passwordArray[] = $randomString[$n];
+        }
+
+        $password = implode($passwordArray);
+
+        if (!$strict) {
+            return $password;
+        }
+
+        //todo may add more symbols later
+        $shuffledSymbols = str_shuffle("@#$%^&*!");
+
+        return substr($password, 0, strlen($password) - 1) . $shuffledSymbols[0];
     }
 }
