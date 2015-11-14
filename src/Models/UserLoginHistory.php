7143d4e28c85e1c44ea9b0855fb188a53f8a8bd1
tega <tega.philip@gmail.com>
2015-11-15 00:49:01 +0100

    TER-13 >> User Login History - Added test cases - Added implementation - Updated phalcon/incubator composer version

---
+++ src/Models/UserLoginHistory.php
@@ -0,0 +1,170 @@
+<?php
+
+namespace UserAuth\Models;
+
+use \Phalcon\Di;
+use UserAuth\Libraries\Utils;
+
+/**
+ * Class UserLoginHistory
+ * @property int id
+ * @property int user_id
+ * @property string date_logged
+ * @property string login_status
+ * @property string ip_address
+ * @property string user_agent
+ * @package UserAuth\Models
+ * @author Tega Oghenekohwo <tega@cottacush.com>
+ */
+class UserLoginHistory extends BaseModel
+{
+
+    const LOGIN_STATUS_SUCCESS = 'success';
+
+    const LOGIN_STATUS_FAILED = 'failed';
+
+    const LOGIN_STATUS_UNKNOWN = 'unknown';
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getUserId()
+    {
+        return $this->user_id;
+    }
+
+    /**
+     * @param int $user_id
+     */
+    public function setUserId($user_id)
+    {
+        $this->user_id = $user_id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getDateLogged()
+    {
+        return $this->date_logged;
+    }
+
+    /**
+     * @param string $date_logged
+     */
+    public function setDateLogged($date_logged)
+    {
+        $this->date_logged = $date_logged;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLoginStatus()
+    {
+        return $this->login_status;
+    }
+
+    /**
+     * @param string $login_status
+     */
+    public function setLoginStatus($login_status)
+    {
+        $this->login_status = $login_status;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIpAddress()
+    {
+        return $this->ip_address;
+    }
+
+    /**
+     * @param string $ip_address
+     */
+    public function setIpAddress($ip_address)
+    {
+        $this->ip_address = $ip_address;
+    }
+
+    /**
+     * @return string
+     */
+    public function getUserAgent()
+    {
+        return $this->user_agent;
+    }
+
+    /**
+     * @param string $user_agent
+     */
+    public function setUserAgent($user_agent)
+    {
+        $this->user_agent = $user_agent;
+    }
+
+    /**
+     * @return string
+     */
+    public function getSource()
+    {
+        return "user_login_history";
+    }
+
+    /**
+     * Set field before validation check
+     */
+    public function beforeValidationOnCreate()
+    {
+        $this->date_logged = Utils::getCurrentDateTime();
+    }
+
+    /**
+     * Save
+     * @param $data
+     * @return bool
+     */
+    public function addLog($data)
+    {
+        /* @var \Phalcon\Http\Request */
+        $request = Di::getDefault()->getRequest();
+
+        if (empty($data['ip_address'])) {
+            $data['ip_address'] = $request->getClientAddress();
+        }
+
+        if (empty($data['user_agent'])) {
+            $data['user_agent'] = $request->getUserAgent();
+        }
+
+        return $this->save($data);
+    }
+
+    /**
+     * Get an instance of this class
+     * @author Tega Oghenekohwo <tega@cottacush.com>
+     * @return $this
+     */
+    public static function getInstance()
+    {
+        return new self();
+    }
+}
